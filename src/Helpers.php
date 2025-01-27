6b29faadf7c3349fe266660f6e15e3f844279b33
Louis Ouellet <louis_ouellet@hotmail.com>
2025-01-27 17:02:50 -0500

    Added Helpers and Models. Helpers for basic shared methods and Models for database shared methods

---
+++ src/Helpers.php
@@ -0,0 +1,67 @@
+<?php
+
+/**
+ * Core Framework - Helpers
+ *
+ * @license    MIT (https://mit-license.org/)
+ * @author     Louis Ouellet <louis@laswitchtech.com>
+ */
+
+// Declaring namespace
+namespace LaswitchTech\Core;
+
+// Import additionnal class into the global namespace
+use Exception;
+
+class Helpers {
+
+    // Properties
+    protected $Path = null;
+    protected array $Helpers = []; // store them here
+
+    public function __construct() {
+
+        // Import Global Variables
+        global $CONFIG;
+
+        // Set Path
+        $this->Path = $CONFIG->root() . "/Helper";
+
+        // Check if the Helper directory exists
+        if(is_dir($this->Path)){
+
+            // Loop through all the files in the directory
+            foreach(scandir($this->Path) as $helper){
+
+                // Check if the file is a Helper
+                if (!preg_match('/(.+)Helper\.php$/i', $helper, $matches)) {
+                    continue;
+                }
+
+                // Include the Helper
+                require_once $this->Path . "/" . $helper;
+
+                // Get the Helper Base Name and Class Name
+                $baseName = $matches[1];
+                $className      = $baseName . 'Helper';
+
+                // Check if the class exists
+                if (class_exists($className)) {
+
+                    // Create the Helper
+                    $this->Helpers[$baseName] = new $className();
+                }
+            }
+        }
+    }
+
+    // Magic getter to retrieve a Helper
+    public function __get($name) {
+        return $this->Helpers[$name] ?? null;
+    }
+
+    // Magic setter to add a Helper
+    public function __set($name, $value) {
+        $this->Helpers[$name] = $value;
+    }
+}
