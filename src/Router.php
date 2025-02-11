9c53f731aa3aa2e63c1e4392df2e37441db67381
Louis Ouellet <louis_ouellet@hotmail.com>
2025-02-11 08:41:37 -0500

    Added the Router module to handle the front-end routing

---
+++ src/Router.php
@@ -0,0 +1,235 @@
+<?php
+
+/**
+ * Core Framework - Router
+ *
+ * @license    MIT (https://mit-license.org/)
+ * @author     Louis Ouellet <louis@laswitchtech.com>
+ */
+
+// Declaring namespace
+namespace LaswitchTech\Core;
+
+// Import additionnal class into the global namespace
+use LaswitchTech\Core\Objects;
+use Exception;
+
+class Router {
+
+    // Constants
+    const HttpCodes = [400,401,403,404,422,423,427,428,429,430,432,500,501];
+    const HttpCustomCodes = [427,430,432];
+    const HttpLabels = [
+        "400" => "Bad Request", // 400 Error Document // Bad Request
+        "401" => "Unauthorized", // 401 Error Document // Unauthorized
+        "403" => "Forbidden", // 403 Error Document // Forbidden
+        "404" => "Not Found", // 404 Error Document // Not Found
+        "422" => "Unprocessable Content", // 422 Error Document // Unprocessable Content
+        "423" => "Locked", // 423 Error Document // Locked
+        "427" => "2FA Required", // 427 Error Document // 2FA Required
+        "428" => "Verification Required", // 428 Error Document // Verification Required
+        "429" => "Too Many Requests", // 429 Error Document // Too Many Requests
+        "430" => "Unauthenticated", // 430 Error Document // Unauthenticated
+        "432" => "Unverified", // 432 Error Document // Unverified
+        "500" => "Internal Server Error", // 500 Error Document // Internal Server Error
+        "501" => "Not Implemented", // 501 Error Document // Not Implemented
+    ];
+
+    // Global Properties
+    private $Config;
+    private $Auth;
+    private $Request;
+    private $Output;
+
+    // Properties
+    private $Route;
+    private $Routes = [];
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        // Global Variables
+        global $CONFIG, $REQUEST, $OUTPUT, $AUTH;
+
+        // Set Global Properties
+        $this->Config = $CONFIG;
+        $this->Request = $REQUEST;
+        $this->Output = $OUTPUT;
+        $this->Auth = $AUTH;
+
+        // Configure Globals
+        $this->Config->add('routes');
+
+        // Check if the server requirements are met
+        $this->checkRequirements();
+
+        // Load Routes
+        $this->load();
+    }
+
+    /**
+     * Check if the required apache modules are installed
+     *
+     * @return self
+     */
+    private function checkRequirements(): self
+    {
+        // Check Server Type
+        if(strpos(strtoupper($this->Request->getParams('SERVER','SERVER_SOFTWARE')), 'APACHE') === false){
+            $this->Output->print(
+                "This application requires an Apache server.",
+                array('HTTP/1.1 500 Internal Error'),
+            );
+        }
+
+        // Check Apache if Mod Rewrite is enabled
+        if(is_null($this->Request->getParams('SERVER','REDIRECT_MOD_REWRITE')) && is_null($this->Request->getParams('SERVER','MOD_REWRITE'))){
+            $this->Output->print(
+                "This application requires the Apache mod_rewrite module.",
+                array('HTTP/1.1 500 Internal Error'),
+            );
+        }
+
+        return $this;
+    }
+
+    /**
+     * Load all routes
+     *
+     * @return self
+     */
+    private function load(): self
+    {
+        // Load Error Routes
+        foreach(self::HttpCodes as $Code){
+            $Code = strval($Code);
+            $this->Routes[$Code] = $this->route($Code, ['label' => self::HttpLabels[$Code], 'view' => $Code]);
+        }
+
+        // Load Routes
+        if($this->Config->get('routes')){
+            foreach($this->Config->get('routes') as $route => $param){
+                $this->Routes[$route] = $this->route($route, $param);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Add/Generate a route
+     *
+     * @param string $route
+     * @param array $data
+     * @return self
+     */
+    public function route(string $route, ?array $data = null): Objects\Route
+    {
+        if(isset($this->Routes[$route])){
+            if(!is_null($data)){
+                $this->Routes[$route]->set($data);
+            }
+            return $this->Routes[$route];
+        }
+        return new Objects\Route($route, $data);
+    }
+
+    /**
+     * Set the current route
+     *
+     * @param string $route
+     * @return self
+     */
+    public function set(string $route): self
+    {
+        if(!isset($this->Routes[$route])){
+
+            // Send Not Found
+            $this->Route = $this->Routes['404'];
+        } else {
+
+            // Check if the route is public
+            if(!$this->Routes[$route]->public()){
+
+                // Check if Auth is available
+                if(!in_array(get_class($this->Auth),["Module","LaswitchTech\Core\Module"])){
+
+                    // Check if the user is loaded
+                    if(!$this->Auth->isLoaded()){
+                        $this->Route = $this->Routes['430'];
+
+                        return $this;
+                    }
+
+                    // Check if the user is authenticated
+                    if(!$this->Auth->isAuthenticated()){
+
+                        // Send Unauthorized
+                        $this->Route = $this->Routes['430'];
+
+                        return $this;
+                    }
+
+                    // Check if the user is deleted
+                    if($this->Auth->user()->deleted()){
+
+                        // Send Unauthorized
+                        $this->Route = $this->Routes['401'];
+
+                        return $this;
+                    }
+
+                    // Check if the user is banned
+                    if($this->Auth->user()->banned()){
+
+                        // Send Forbidden
+                        $this->Route = $this->Routes['403'];
+
+                        return $this;
+                    }
+
+                    // Check if the user is verified
+                    if(!$this->Auth->user()->verified()){
+
+                        // Send Unverified
+                        $this->Route = $this->Routes['428'];
+
+                        return $this;
+                    }
+
+                    // Check if the user has the required permission
+                    if(!$this->Auth->isAuthorized('Route>' . $this->Routes[$route]->namespace(), $this->Routes[$route]->level())){
+
+                        // Send Forbidden
+                        $this->Route = $this->Routes['403'];
+
+                        return $this;
+                    }
+
+                    // Set the route
+                    $this->Route = $this->Routes[$route];
+                }
+            } else {
+
+                // Set the route
+                $this->Route = $this->Routes[$route];
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Start the router
+     *
+     * @return self
+     */
+    public function start(): self
+    {
+        $this->set($this->Request->getNamespace());
+        $this->Route->render();
+        return $this;
+    }
+}
