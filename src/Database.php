5c66cd75479f209a080167b3f4ba7993bd47bf62
Louis Ouellet <louis_ouellet@hotmail.com>
2025-03-03 21:56:58 -0500

    Added the method isInstalled() and install() to the Auth module to perform installation tasks

--- src/Database.php
+++ src/Database.php
@@ -112,4 +112,203 @@ class Database {
     {
         return new Objects\Schema($this->connector);
     }
+
+    /**
+     * Check if the module is installed
+     *
+     * @return bool
+     */
+    public function isInstalled(): bool
+    {
+        return $this->Config->reload('database')->get('database', 'installed');
+    }
+
+    /**
+     * Install the module
+     *
+     * @param array $config
+     * @return array
+     */
+    public function install(array $config): array
+    {
+        // Initialize the status
+        $status = [];
+
+        // Check if the config includes all the required fields
+        if(isset($config['connector'],$config['host'],$config['database'],$config['username'],$config['password'])){
+
+            // Save the settings
+            $this->Config->set('database', 'connector', $config['connector']);
+            $this->Config->set('database', 'host', $config['host']);
+            $this->Config->set('database', 'database', $config['database']);
+            $this->Config->set('database', 'username', $config['username']);
+            $this->Config->set('database', 'password', $config['password']);
+
+            // Connect to the database
+            $this->connect();
+
+            // Check if the database is connected
+            if($this->isConnected()){
+
+                // Retrieve the current version
+                $version = $this->Config->version();
+
+                // Retrieve the path of the version's directory
+                $path = $this->Config->root() . DIRECTORY_SEPARATOR . "Update" . DIRECTORY_SEPARATOR . $version;
+
+                // Check if the directory exists
+                if(is_dir($path)) {
+
+                    // Retrive the list of definition files
+                    $definitions = array_diff(scandir($path . DIRECTORY_SEPARATOR . "Definition"), array('..', '.'));
+
+                    // Check if the Definition directory exists if not create it
+                    if(!is_dir($this->Config->root() . DIRECTORY_SEPARATOR . "Definition")){
+                        mkdir($this->Config->root() . DIRECTORY_SEPARATOR . "Definition", 0755, true);
+                    }
+
+                    // Loop through the definition files
+                    foreach($definitions as $definition) {
+
+                        // Remove the .map extension
+                        $definition = str_replace('.map', '', $definition);
+
+                        // Check if the definition file already exists and delete it
+                        if(is_file($this->Config->root() . DIRECTORY_SEPARATOR . "Definition" . DIRECTORY_SEPARATOR . $definition . ".map")){
+                            unlink($this->Config->root() . DIRECTORY_SEPARATOR . "Definition" . DIRECTORY_SEPARATOR . $definition . ".map");
+                        }
+
+                        // Copy the definition file to the Definition directory
+                        copy($path . DIRECTORY_SEPARATOR . "Definition" . DIRECTORY_SEPARATOR . $definition . ".map", $this->Config->root() . DIRECTORY_SEPARATOR . "Definition" . DIRECTORY_SEPARATOR . $definition . ".map");
+
+                        // Create the Schema
+                        $Schema = $this->schema()->define($definition);
+
+                        // Check if the Schema is already exists
+                        if($Schema->exists()){
+
+                            // Drop the Schema
+                            $Schema->drop();
+                        }
+
+                        // Import the Schema in the Database
+                        $Schema->create();
+
+                        // Check if the data file exists
+                        if(is_file($path . DIRECTORY_SEPARATOR . "Data" . DIRECTORY_SEPARATOR . $definition . ".required")){
+
+                            // Retrieve the content of the data file
+                            $records = json_decode(file_get_contents($path . DIRECTORY_SEPARATOR . "Data" . DIRECTORY_SEPARATOR . $definition . ".required"),true);
+
+                            // Loop through the records
+                            foreach($records as $record){
+
+                                // Set Record
+                                $record['owner'] = $config['username'];
+                                $record['created'] = date('Y-m-d H:i:s');
+                                $record['modified'] = date('Y-m-d H:i:s');
+
+                                // Create the Query
+                                $Query = $this->query()
+                                    ->table($definition)
+                                    ->insert($record)
+                                    ->execute();
+                            }
+
+                            // Check if the data file exists
+                            $status[] = true;
+
+                            // Check if sample data should be loaded
+                            if(isset($config['sample']) && filter_var($config['sample'], FILTER_VALIDATE_BOOLEAN)){
+
+                                // Check if the data file exists
+                                if(is_file($path . DIRECTORY_SEPARATOR . "Data" . DIRECTORY_SEPARATOR . $definition . ".sample")){
+
+                                    // Retrieve the content of the data file
+                                    $records = json_decode(file_get_contents($path . DIRECTORY_SEPARATOR . "Data" . DIRECTORY_SEPARATOR . $definition . ".sample"),true);
+
+                                    // Loop through the records
+                                    foreach($records as $record){
+
+                                        // Set Record
+                                        $record['owner'] = $config['username'];
+                                        $record['created'] = date('Y-m-d H:i:s');
+                                        $record['modified'] = date('Y-m-d H:i:s');
+
+                                        // Create the Query
+                                        $Query = $this->query()
+                                            ->table($definition)
+                                            ->insert($record)
+                                            ->execute();
+                                    }
+
+                                    // Check if the data file exists
+                                    $status[] = true;
+                                } else {
+                                    $status[] = "Could not find the sample data file";
+                                }
+                            }
+
+                            // Set default auto increment
+                            $autoIncrement = 10000;
+
+                            // Check if preload data should be loaded
+                            if(isset($config['preload']) && filter_var($config['preload'], FILTER_VALIDATE_BOOLEAN)){
+
+                                // Check if the data file exists
+                                if(is_file($path . DIRECTORY_SEPARATOR . "Data" . DIRECTORY_SEPARATOR . $definition . ".preload")){
+
+                                    // Retrieve the content of the data file
+                                    $records = json_decode(file_get_contents($path . DIRECTORY_SEPARATOR . "Data" . DIRECTORY_SEPARATOR . $definition . ".preload"),true);
+
+                                    // Loop through the records
+                                    foreach($records as $record){
+
+                                        // Set Record
+                                        $record['owner'] = $config['username'];
+                                        $record['created'] = date('Y-m-d H:i:s');
+                                        $record['modified'] = date('Y-m-d H:i:s');
+
+                                        // Create the Query
+                                        $Query = $this->query()
+                                            ->table($definition)
+                                            ->insert($record);
+
+                                        // Execute the Query
+                                        $Query->execute();
+
+                                        // Set auto increment
+                                        $autoIncrement = ($Query->lastId() + 1);
+                                    }
+                                } else {
+                                    $status[] = "Could not find the preload data file";
+                                }
+                            }
+
+                            // Set auto increment on the table
+                            $this->query()->table($definition)->autoIncrement($autoIncrement);
+
+                            // Set the database module as installed
+                            $this->Config->set('database', 'installed', true);
+
+                            // Add a true status
+                            $status[] = true;
+                        } else {
+                            $status[] = "Could not find the data file";
+                        }
+                    }
+                } else {
+                    $status[] = "Could not find the update directory";
+                }
+            } else {
+                $this->Config->delete('database');
+                $status[] = "Could not connect to the database";
+            }
+        } else {
+            $status[] = "Missing required fields";
+        }
+
+        // Return the statuses
+        return $status;
+    }
 }
