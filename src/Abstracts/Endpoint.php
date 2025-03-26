da4a6eb6713e686c25290ffad62027f431d83fdb
Louis Ouellet <louis_ouellet@hotmail.com>
2025-03-26 16:06:54 -0400

    Adding the Config module into the Endpoint for easier access

--- src/Abstracts/Endpoint.php
+++ src/Abstracts/Endpoint.php
@@ -21,6 +21,7 @@ abstract class Endpoint {
     protected $Helper;
     protected $Output;
     protected $Request;
+    protected $Config;
 
     // Auth Properties
     protected $Level;
@@ -33,7 +34,7 @@ abstract class Endpoint {
     {
 
         // Import Global Variables
-        global $AUTH, $MODEL, $HELPER, $OUTPUT, $REQUEST;
+        global $AUTH, $MODEL, $HELPER, $OUTPUT, $REQUEST, $CONFIG;
 
         // Initialize Properties
         $this->Auth = $AUTH;
@@ -41,6 +42,7 @@ abstract class Endpoint {
         $this->Helper = $HELPER;
         $this->Output = $OUTPUT;
         $this->Request = $REQUEST;
+        $this->Config = $CONFIG;
     }
 
     /**
