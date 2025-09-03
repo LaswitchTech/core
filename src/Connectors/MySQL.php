6241abcad92159d5d4befd8718f11c7531d4ba01
Louis Ouellet <louis_ouellet@hotmail.com>
2025-09-03 10:19:34 -0400

    General: Code Clean-up

--- src/Connectors/MySQL.php
+++ src/Connectors/MySQL.php
@@ -1,12 +1,5 @@
 <?php
 
-/**
- * Core Framework - MySQL
- *
- * @license    MIT (https://mit-license.org/)
- * @author     Louis Ouellet <louis@laswitchtech.com>
- */
-
 // Declaring namespace
 namespace LaswitchTech\Core\Connectors;
 
