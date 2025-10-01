7e3be7fcf36bf80cbcb80991d209fd25c1f3ca1f
Louis Ouellet <louis_ouellet@hotmail.com>
2025-10-01 09:03:30 -0400

    General: Code Obtimisation.

--- Helper/CoreHelper.php
+++ Helper/CoreHelper.php
@@ -641,16 +641,20 @@ class CoreHelper extends Helper {
             return false;
         }
 
+        // Loop through each item in the directory
         foreach ($items as $item) {
+
             // Skip pointers
-            if ($item === '.' || $item === '..') {
+            if (in_array($item,['.','..'])) {
                 continue;
             }
 
+            // Full path to the item
             $path = $directory . DIRECTORY_SEPARATOR . $item;
 
             // Recursively call delete on each item
             if (!$this->delete($path)) {
+
                 // If any item fails to be deleted, return false
                 return false;
             }
