1c337703809f1cbc33e451d1efa310a6d94da780
Louis Ouellet <louis_ouellet@hotmail.com>
2025-10-16 12:47:44 -0400

    BUGFIX: The Database does not need to be provided during construction.

--- Model/CoreModel.php
+++ Model/CoreModel.php
@@ -250,7 +250,7 @@ class coreModel extends Model {
                                     if(class_exists($postClass)){
 
                                         // Initialize the Post Model
-                                        $PostModel = new $postClass($this->Database);
+                                        $PostModel = new $postClass();
 
                                         // Check if the post method exists
                                         if(method_exists($PostModel, 'post')){
