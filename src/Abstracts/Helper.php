45775efdc9eacb7f03747458ea29c6474e0aef35
Louis Ouellet <louis_ouellet@hotmail.com>
2026-06-01 07:48:58 -0400

    Code Cleanup

--- src/Abstracts/Helper.php
+++ src/Abstracts/Helper.php
@@ -1,12 +1,5 @@
 <?php
 
-/**
- * Core Framework - Helper
- *
- * @license    MIT (https://mit-license.org/)
- * @author     Louis Ouellet <louis@laswitchtech.com>
- */
-
 // Declaring namespace
 namespace LaswitchTech\Core\Abstracts;
 
